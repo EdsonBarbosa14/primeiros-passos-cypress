27292da413502414b11a65c2c3053a78ec51bdd4
Edson Barbosa <edson.barbosa.junior14@gmail.com>
2024-12-06 01:56:09 -0300

    Alocando automação de login em uma classe.

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,44 +1,34 @@
-import userData from '../fixtures/user-data.json'
 
+import loginClass from '../helpers/loginPage.js'
 
 describe('Orange HRM Tests', () => {
-    const selectorsList = {
-      urlLogin: "/auth/login",
-      usernameField: "[name='username']", 
-      passwordField: "[name='password']",
-      loginButton: "[type='submit']",
-      sectionTittleTopBar: ".oxd-topbar-header-breadcrumb-module",
-      dashboardGrid: ".orangehrm-dashboard-grid",
-      wrongCredentialAlert: "[role='alert']",
-      MyInfoMenuButton: ":nth-child(6) > .oxd-main-menu-item",
-    }
+  const urlLogin = "/auth/login"
+  
+  const stepsLogin = new loginClass()
 
-    const dataMyInfo =
-    {
-        firstNamField: "[name='firstName']",
-        middleNameField: "[name='middleName']",
-        lastNameField: "[name='lastName']",
-        employeeIdField:":nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
-        otherIdField:":nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input",
-        driverLicenseNumberField: ":nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
-    }
+  const dataMyInfo = {
+      MyInfoMenuButton: ":nth-child(6) > .oxd-main-menu-item",
+      firstNamField: "[name='firstName']",
+      middleNameField: "[name='middleName']",
+      lastNameField: "[name='lastName']",
+      employeeIdField:":nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
+      otherIdField:":nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input",
+      driverLicenseNumberField: ":nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
+      dateField: ':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input',
+  }
   
-    it.only('User Info Update - Sucess', () => {
-      cy.visit(selectorsList.urlLogin)
-      cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-      cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-      cy.get(selectorsList.loginButton).click()
-      cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-      cy.get(selectorsList.dashboardGrid)
-      cy.get(selectorsList.MyInfoMenuButton).click()
-      cy.location('pathname').should('equal', '/web/index.php/pim/viewPersonalDetails/empNumber/7')
-      cy.get(dataMyInfo.firstNamField).type('ju')
-      cy.get(dataMyInfo.middleNameField).type('ni')
-      cy.get(dataMyInfo.lastNameField).type('nho')
-      cy.get(dataMyInfo.employeeIdField).type('1111')
-      cy.get(dataMyInfo.otherIdField).type('222')
-      cy.get(dataMyInfo.driverLicenseNumberField).type('40303034020202')
-    })
-  
-   
+  it.only('User Info Update - Sucess', () => {
+    cy.visit(urlLogin)
+    stepsLogin.makeLogin()
+    cy.get(dataMyInfo.MyInfoMenuButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/pim/viewPersonalDetails/empNumber/7')
+    cy.get(dataMyInfo.firstNamField).clear().type('Jorge')
+    cy.get(dataMyInfo.middleNameField).clear().type('Carvalho')
+    cy.get(dataMyInfo.lastNameField).clear().type('dos Santos')
+    cy.get(dataMyInfo.employeeIdField).clear().type('1111')
+    cy.get(dataMyInfo.otherIdField).clear().type('222')
+    cy.get(dataMyInfo.driverLicenseNumberField).clear().type('40303034020202')
+    cy.get(dataMyInfo.dateField).clear().type('2025-03-10')
   })
+     
+})
---
+++ cypress/helpers/loginPage.js
@@ -0,0 +1,23 @@
+import userData from '../fixtures/user-data.json'
+
+const selectorsList = {
+    urlLogin: "/auth/login",
+    usernameField: "[name='username']", 
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTittleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    wrongCredentialAlert: "[role='alert']",
+  }
+
+class loginClass {
+  makeLogin(){
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid)
+  }
+}
+
+export default loginClass
